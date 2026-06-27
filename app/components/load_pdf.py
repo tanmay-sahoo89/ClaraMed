447e9c115f2f2c1f7a0ea17ac58ffa9868d48ba0
Tanmay Chiranjib Sahoo <sahootanmay2005@gmail.com>
2026-06-27 12:04:39 +0530

    text_chunk func done

--- app/components/load_pdf.py
+++ app/components/load_pdf.py
@@ -20,7 +20,6 @@ def load_pdf_files():
         )
         
         documents = loader.load() #pdf stored in this
-        
         if not documents:
             logger.warning("No PDF's were found.")
         else:
@@ -32,4 +31,20 @@ def load_pdf_files():
         return []
     
 def create_text_chunks(documents):
-    
+    try:
+        if not documents:
+            raise CustomException("No documents were found.")
+        logger.info(f"Splitting {len(documents)} documents into chunks.")
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = CHUNK_SIZE,
+            chunk_overlap = CHUNK_OVERLAP
+        )
+        
+        text_chunks = text_splitter.split_documents(documents)
+        logger.info(f"Generated {len(text_chunks)} text chunks.")
+        return text_chunks
+    except Exception as e:
+        error_message = CustomException("Failed to generate text chunks", e)
+        logger.error(str(error_message))
+        return []
