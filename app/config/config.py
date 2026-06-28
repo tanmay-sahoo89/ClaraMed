ae1a26831fe5656b80512da4a81e8fd04e9a8a3c
Tanmay Chiranjib Sahoo <sahootanmay2005@gmail.com>
2026-06-28 11:46:02 +0530

    logger done

--- app/config/config.py
+++ app/config/config.py
@@ -5,5 +5,5 @@ GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 DB_FAISS_PATH = "vectorstore/db_faiss"
 DATA_PATH = "data/"
 
-CHUNK_SIZE = 500
-CHUNK_OVERLAP = 50
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 150
